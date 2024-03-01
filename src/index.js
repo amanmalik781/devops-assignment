315fc82e32b3ffd0b6b7d7c165016cffe6141059
AMAN DEEP MALIK <36561468+amanmalik781@users.noreply.github.com>
2024-03-01 17:03:27 +0530

    es6 function syntax

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-function calculateFactorial(n) {
+const calculateFactorial = (n) => {
     if (n === 0 || n === 1) {
         return 1;
     } else {
         return n * calculateFactorial(n - 1);
     }
-}
+};
 
 module.exports = {
     calculateFactorial
